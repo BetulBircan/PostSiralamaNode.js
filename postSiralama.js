db719d5627429f612c99c8fe4dc0ee33b3dd3d6e
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-02-24 01:58:36 +0300

    async-awaityapısından yararlanılarak eklediğimiz postla birlikte sırayla listeleme işlemi yapıldı.

--- postSiralama.js
+++ postSiralama.js
@@ -25,3 +25,19 @@ const addPosts = (newPost) => {
     return promise1
 }
 
+//eklenen kitapla birlikte postların sıralı bir şekilde listelenmesi-Pluşturulam promise i karşılayabilmesi için async-await yapısı kullanıldı. Async-await yapısının tercih edilmesinin sebebi bu örnekteki asenkron yapıda olan işlemleri keskin sıralı bir şekilde listeleyebilmesi için hem de yaımı daha kolay olduğu için:
+
+async function showPosts() {
+    try {
+        //await dediğimizde posttaki ekleme işlemi bitmeden listeleme işlemine geçmesini önlemiş oluruz yani işlemin bitmesini bekler.
+        await addPosts({ postName: "Gönderi 5", subject: "Konu 4", text: "Metin 5" })
+        listPosts()
+    }
+    //hata oluşması durumunda catch bloğu çalışır.
+    catch(error) {
+        console.log(error)
+    }
+} 
+
+showPosts()
+

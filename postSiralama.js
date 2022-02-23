94234de619a721391293345436c4980b990e7f7a
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-02-24 01:46:31 +0300

    array yapısı ile postlar oluşturuldu.

--- postSiralama.js
+++ postSiralama.js
@@ -1,3 +1,10 @@
 //Blog oluşturmaya hazır mısınız? Konsol ekranında postlarımızı sıralayalım, sonrasında yeni bir post oluşturalım ve yeni post ile birlikte postlarımızı tekrar sıralayalım.
 
+//Post eleamları tanımlama-Array içerisinde obje kullanarak
+const posts = [
+    { postName: "Gönderi 1", subject: "Konu 1", text: "Metin 1" },
+    { postName: "Gönderi 2", subject: "Konu 2", text: "Metin 2" },
+    { postName: "Gönderi 3", subject: "Konu 3", text: "Metin 3" },
+    { postName: "Gönderi 4", subject: "Konu 4", text: "Metin 4" }
+]
 
